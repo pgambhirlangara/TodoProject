cb0cc0153033d46af87516ca357557fcc136f876
Prabhjyot <knowprabhjyot@gmail.com>
2022-03-26 14:28:58 -0700

    Initial project structure

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Server working at port ${PORT}`);
+});
+
